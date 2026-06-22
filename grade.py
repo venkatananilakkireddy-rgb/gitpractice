d0c8cb60d3ea691b21076f07a521bbd5f0e03cf6
LAKKI REDDY VENKATA NANI <venkatananilakkireddy@gmail.com>
2026-06-22 19:00:06 +0530

    Added grade calculator

---
+++ grade.py
@@ -0,0 +1,11 @@
+name = input("Enter student name: ")
+marks = float(input("Enter marks: "))
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 60:
+    print("Grade C")
+else:
+    print("Fail")
