ac27c603bb5bf12b962834696f68ede7647f77da
LAKKI REDDY VENKATA NANI <venkatananilakkireddy@gmail.com>
2026-06-23 16:35:02 +0530

    added student result Analyzer

---
+++ student_result.py
@@ -0,0 +1,9 @@
+marks = [85, 90, 78, 95, 88]
+
+total = sum(marks)
+average = total / len(marks)
+
+print("Total Marks =", total)
+print("Average Marks =", average)
+print("Highest Marks =", max(marks))
+print("Lowest Marks =", min(marks))
